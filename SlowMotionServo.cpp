92ba864df568c9da3639a1710808312677fe77ea
Koryphon <Koryphon@users.noreply.github.com>
2015-03-24 17:01:10 +0100

    in progress

---
+++ SlowMotionServo.cpp
@@ -0,0 +1,27 @@
+/*
+ * Slow Motion Servo Library for Arduino
+ * 
+ * Copyright Jean-Luc Béchennec 2015
+ *
+ * This software is distributed under the GNU Public Licence v2 (GPLv2)
+ *
+ * Please read the LICENCE file
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+
+#include <SlowMotionServo.h>
+
+SlowMotionServo *SlowMotionServo::sServoList = NULL;
+
+void SlowMotionServo::setMinMax(int minPulse, int maxPulse)
+{
+  if (
+void SlowMotionServo::setMin(int minPulse);
+void SlowMotionServo::setMax(int maxPulse);
